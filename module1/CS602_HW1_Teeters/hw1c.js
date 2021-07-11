d5e84a6c57823f6e0819bc9405d8f1d38a624e03
Lili Teeters <43280648+LiliTeeters@users.noreply.github.com>
2021-07-11 12:18:15 -0600

    keep trying

--- module1/CS602_HW1_Teeters/hw1c.js
+++ module1/CS602_HW1_Teeters/hw1c.js
@@ -4,5 +4,21 @@ const ZipCodeEmitter = require('./zipCodeEmitter').ZipCodeEmitter;
 
 const cities = new ZipCodeEmitter();
 
+cities.on('lookupByZipCode', (index) => {
+    console.log(colors.green('\nEvent lookupByZipCode raised! \n', index));
+});
+
+cities.on('lookupByCityState', (results) => {
+    console.log('\nEvent lookupByCityState raised! (Handler1) \n', results);
+});
+
+cities.on('getPopulationByState', (results) => {
+    console.log('\nEvent getPopulationByState raised! (Handler2) \n', results);
+});
+
+cities.lookupByZipCode('02215');
+cities.lookupByCityState("BOSTON", "MA");
+cities.getPopulationByState('MA');
+
 
 
--- module1/CS602_HW1_Teeters/zipCodeEmitter.js
+++ module1/CS602_HW1_Teeters/zipCodeEmitter.js
@@ -7,15 +7,33 @@ class ZipCodeEmitter  extends EventEmitter {
 	// member functions
 
 	lookupByZipCode(zip)  {
-	
+		for(let index = 0; index < data.length; index++) {
+			if(data[index]._id === zip) {
+				this.emit('lookupByZipCode', data[index]);
+			}
+		}
 	}
 
 	lookupByCityState(city, state)  {
-	
+		const newData = []
+    	let results = {'city': city, 'state': state, 'data': newData};
+    	for(let index = 0; index < data.length; index++){
+        	if((data[index].city === city) && (data[index].state === state)){
+            newData.push({'zip': data[index]._id, 'pop': data[index].pop}	)
+        	}
+    	}	 
+    	this.emit('lookupByCityState', results) ;	
 	}
 
 	getPopulationByState(state) {
-	
+		let population = 0;
+    	for(let index = 0; index < data.length; index++){
+        	if(data[index].state === state){
+            population += data[index].pop;   
+        	}
+    	}
+    	let results = {'state': state, 'pop': population};
+    	this.emit('getPopulationByState', results);	
 	}
 
 }
