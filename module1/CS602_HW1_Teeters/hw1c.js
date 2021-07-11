efcb49a8b1711c0eadc1407e1a23467d9cee9bf7
Lili Teeters <43280648+LiliTeeters@users.noreply.github.com>
2021-07-11 13:23:30 -0600

    final

--- module1/CS602_HW1_Teeters/hw1c.js
+++ module1/CS602_HW1_Teeters/hw1c.js
@@ -5,19 +5,25 @@ const ZipCodeEmitter = require('./zipCodeEmitter').ZipCodeEmitter;
 const cities = new ZipCodeEmitter();
 
 cities.on('lookupByZipCode', (index) => {
-    console.log(colors.green('\nEvent lookupByZipCode raised! \n', index));
+    console.log(colors.yellow('Event lookupByZipCode raised!\n', index));
 });
 
 cities.on('lookupByCityState', (results) => {
-    console.log('\nEvent lookupByCityState raised! (Handler1) \n', results);
+    console.log(colors.yellow('Event lookupByCityState raised! (Handler1)\n', results));
 });
 
 cities.on('getPopulationByState', (results) => {
-    console.log('\nEvent getPopulationByState raised! (Handler2) \n', results);
+    console.log(colors.yellow('Event getPopulationByState raised! (Handler2)\n', results));
 });
 
+console.log("\nLookupByZipCode (02215)".green);
 cities.lookupByZipCode('02215');
+
+console.log("\nLookupByCityState (BOSTON,MA)".green);
 cities.lookupByCityState("BOSTON", "MA");
+
+
+console.log("\ngetPopulationByState (MA)".green);
 cities.getPopulationByState('MA');
 
 
