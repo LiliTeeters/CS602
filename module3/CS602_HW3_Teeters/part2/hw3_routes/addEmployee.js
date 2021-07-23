0d4424d5bec0d42065d69f9a988569f7e548c8a6
Lili Teeters <43280648+LiliTeeters@users.noreply.github.com>
2021-07-22 21:45:38 -0600

    moving along

--- module3/CS602_HW3_Teeters/part2/hw3_routes/addEmployee.js
+++ module3/CS602_HW3_Teeters/part2/hw3_routes/addEmployee.js
@@ -1,6 +1,6 @@
 module.exports = (req , res , next) => {
 		
 		// Fill in the code
-		res.render('addEmployeeView');
+		res.render('addEmployeeView', {title:"Add a New Employee"});
 
 };
--- module3/CS602_HW3_Teeters/part2/hw3_routes/deleteEmployee.js
+++ module3/CS602_HW3_Teeters/part2/hw3_routes/deleteEmployee.js
@@ -4,7 +4,16 @@ const Employee = employeeDB.getModel();
 module.exports = async (req , res , next) => {
     
     // Fill in the code
-        
-  };
+    let id = req.params.id;
+    let deleteEmployee = Employee({
+      id
+    });
+    let result = await deleteEmployee.remove()
+    console.log(result)
+    
+    res.redirect('/deleteEmployee');
+    }
+  
+      
 
   
