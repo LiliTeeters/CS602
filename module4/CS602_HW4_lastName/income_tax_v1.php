ebd51f65cfa1c6daeedd260df29bca4dc33bdd6a
Lili Teeters <43280648+LiliTeeters@users.noreply.github.com>
2021-07-28 16:08:13 -0600

    saving

--- module4/CS602_HW4_lastName/income_tax_v1.php
+++ module4/CS602_HW4_lastName/income_tax_v1.php
@@ -5,8 +5,28 @@
 
 function incomeTaxSingle($taxableIncome) {
     $incTax = 0.0;
+    if($taxableIncome >= 0 && $taxableIncome <= 9700) {
+        $incTax = ($taxableIncome * 0.10);
 
-    
+    }
+    elseif ($taxableIncome >= 9700 && $taxableIncome <= 39475) {
+        $incTax = incomeTaxRate($taxableIncome, 970, 12, 9699);
+    } 
+    elseif ($taxableIncome >= 39475 && $taxableIncome <= 84200) {
+        $incTax = incomeTaxRate($taxableIncome, 4543, 22, 39474);
+    } 
+    elseif ($taxableIncome >= 84200 && $taxableIncome <= 160725) {
+        $incTax = incomeTaxRate($taxableIncome, 14382, 24, 84199);
+    } 
+    elseif ($taxableIncome >= 160725 && $taxableIncome <= 204100) {
+        $incTax = incomeTaxRate($taxableIncome, 32748, 32, 160724);
+    } 
+    elseif ($taxableIncome >= 204100 && $taxableIncome <= 510300) {
+        $incTax = incomeTaxRate($taxableIncome, 46628, 35, 204099);
+    } 
+    elseif ($taxableIncome >= 510300) {
+        $incTax = incomeTaxRate($taxableIncome, 153798, 37, 510299);
+    }
     return $incTax;
 
 }
@@ -14,14 +34,55 @@ function incomeTaxSingle($taxableIncome) {
 function incomeTaxMarriedJointly($taxableIncome) {
     $incTax = 0.0;
 
-    
+    if($taxableIncome >= 0 && $taxableIncome <= 19400) {
+        $incTax = ($taxableIncome * 0.10);
+    }
+    elseif ($taxableIncome >= 19400 && $taxableIncome <= 78950) {
+        $incTax = incomeTaxRate($taxableIncome, 1940, 12, 19399);
+    } 
+    elseif ($taxableIncome >= 78950 && $taxableIncome <= 168400) {
+        $incTax = incomeTaxRate($taxableIncome, 9086, 22, 78949);
+    } 
+    elseif ($taxableIncome >= 168400 && $taxableIncome <= 321450) {
+        $incTax = incomeTaxRate($taxableIncome, 28765, 24, 168399);
+    } 
+    elseif ($taxableIncome >= 321450 && $taxableIncome <= 408200) {
+        $incTax = incomeTaxRate($taxableIncome, 65497, 32, 321449);
+    } 
+    elseif ($taxableIncome >= 408200 && $taxableIncome <= 612350) {
+        $incTax = incomeTaxRate($taxableIncome, 93257, 35, 408199);
+    } 
+    elseif ($taxableIncome >= 612350) {
+        $incTax = incomeTaxRate($taxableIncome, 164709, 37, 612349);
+    }
+
     return $incTax;
 
 }
 
 function incomeTaxMarriedSeparately($taxableIncome) {
     $incTax = 0.0;
-
+    if($taxableIncome >= 0 && $taxableIncome <= 9700) {
+        $incTax = ($taxableIncome * 0.10);
+    }
+    elseif ($taxableIncome >= 9700 && $taxableIncome <= 39475) {
+        $incTax = incomeTaxRate($taxableIncome, 970, 12, 9699);
+    } 
+    elseif ($taxableIncome >= 39475 && $taxableIncome <= 84200) {
+        $incTax = incomeTaxRate($taxableIncome, 4543, 22, 39474);
+    } 
+    elseif ($taxableIncome >= 84200 && $taxableIncome <= 160725) {
+        $incTax = incomeTaxRate($taxableIncome, 14382.50, 24, 84199);
+    } 
+    elseif ($taxableIncome >= 160725 && $taxableIncome <= 204100) {
+        $incTax = incomeTaxRate($taxableIncome, 32748.50, 32, 160724);
+    } 
+    elseif ($taxableIncome >= 204100 && $taxableIncome <= 306175) {
+        $incTax = incomeTaxRate($taxableIncome, 46628.50, 35, 204099);
+    } 
+    elseif ($taxableIncome >= 306175) {
+        $incTax = incomeTaxRate($taxableIncome, 82354.75, 37, 306174);
+    }
     
     return $incTax;
 
@@ -29,10 +90,39 @@ function incomeTaxMarriedSeparately($taxableIncome) {
 
 function incomeTaxHeadOfHousehold($taxableIncome) {
     $incTax = 0.0;
+    if($taxableIncome >= 0 && $taxableIncome <= 19400) {
+        $incTax = ($taxableIncome * 0.10);
+    }
+    elseif ($taxableIncome >= 19400 && $taxableIncome <= 78950) {
+        $incTax = incomeTaxRate($taxableIncome, 1940, 12, 19399);
+    } 
+    elseif ($taxableIncome >= 78950 && $taxableIncome <= 168400) {
+        $incTax = incomeTaxRate($taxableIncome, 9086, 22, 78949);
+    } 
+    elseif ($taxableIncome >= 168400 && $taxableIncome <= 321450) {
+        $incTax = incomeTaxRate($taxableIncome, 28765, 24, 168399);
+    } 
+    elseif ($taxableIncome >= 321450 && $taxableIncome <= 408200) {
+        $incTax = incomeTaxRate($taxableIncome, 65497, 32, 321449);
+    } 
+    elseif ($taxableIncome >= 408200 && $taxableIncome <= 612350) {
+        $incTax = incomeTaxRate($taxableIncome, 93257, 35, 408199);
+    } 
+    elseif ($taxableIncome >= 612350) {
+        $incTax = incomeTaxRate($taxableIncome, 164709, 37, 612349);
+    }
 
     
     return $incTax;
 
+}
+
+function incomeTaxRate($taxableIncome, $minimumTax, $taxRate, $amountOver) {
+    // convert tax bracket to a percentage
+    $taxRate = $taxRate / 100;
+    // calculate the marginal tax rate
+    $marginalTaxRate = (($taxableIncome - $amountOver) * $taxRate) + $minimumTax;
+    return $marginalTaxRate;
 }
 
 ?>
@@ -41,7 +131,7 @@ function incomeTaxHeadOfHousehold($taxableIncome) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>HW4 Part1 - LastName</title>
+    <title>HW4 Part1 - Teeters</title>
 
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
